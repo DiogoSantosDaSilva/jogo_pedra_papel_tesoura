8894394d45c2f2185f06752ad2340f0180fc7887
Diogo Santos <diogosantosdasilva364@gmail.com>
2023-06-04 01:53:50 -0300

    Divisão da janela
    Aqui nos especificamos os dois frames, configuramos a sua posição, largura cor e etc...

--- main.py
+++ main.py
@@ -11,9 +11,23 @@ co4 = "#34eb3d"   # green / verde
 co5 = "#e85151"   # red / vermelha
 fundo = "#3b3b3b"
 
+
+# Configurando a janela
 janela = Tk()
 janela.title('')
 janela.geometry('260x280')
 janela.configure(bg=fundo)
+
+
+# Dividindo a janela
+frame_cima = Frame(janela, width=260, height=100, bg=co1, relief='raised')
+frame_cima.grid(row=0, column=0, sticky=NW)
+
+frame_baixo = Frame(janela, width=260, height=300, bg=co0, relief='flat')
+frame_baixo.grid(row=1, column=0, sticky=NW)
+
+estilo = ttk.Style(janela)
+estilo.theme_use('clam')
+
 
 janela.mainloop()
